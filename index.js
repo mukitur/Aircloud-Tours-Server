f42f8258239cd55e7f0e0634bc25fc63c4dd939d
mukitur <mukit.inway@gmail.com>
2021-10-29 14:31:04 +0600

    Add services done

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors =require('cors');
 
-
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,7 +13,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.adlv3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-//console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run(){
@@ -25,11 +22,13 @@ async function run(){
         const database = client.db("AirCloudTours");
         const servicesCollection = database.collection('services');
 
-        //GET API 
-        app.get('/services', async (req, res) =>{
-            const cursor = servicesCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
+        //POST API
+        app.post('/services', async (req, res) =>{
+            const service = req.body;
+           // console.log('hitting the api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.send(result)
         });
     }
     finally{
